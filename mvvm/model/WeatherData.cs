f5d831e937123d384b4a7974851c63dc1c0d9435
Kingdawnage <katchymemo@yahoo.com>
2024-02-21 15:55:47 +0100

    Added dependencies, UI and API caller

---
+++ mvvm/model/WeatherData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Weather.mvvm.model
+{
+    internal class WeatherData
+    {
+        public static async Task<DataClass.Rootobject> LoadData(string search)
+        {
+            string url = $"https://api.weatherapi.com/v1/current.json?key=382c30f637a04587915104833241502&q={Uri.EscapeDataString(search)}";
+
+            try
+            {
+                var response = await ApiHelper.ApiClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var value = await response.Content.ReadFromJsonAsync<DataClass.Rootobject>();
+
+                Console.Write(value);
+
+                return value;
+
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"HTTP request failed {ex.Message}");
+                throw;
+            }
+            catch (JsonException ex)
+            {
+                MessageBox.Show($"JSON decentralization failed {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured {ex.Message}");
+                throw;
+            }
+
+        }
+    }
+}
---
+++ mvvm/viewmodel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.mvvm.model;
+
+namespace Weather.mvvm.viewmodel
+{
+    public partial class MainViewModel : ObservableRecipient
+    {
+        public HomeViewModel HomeVM { get; set; }
+
+        [ObservableProperty]
+        private object currentView;
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+
+        [ObservableProperty]
+        private string searchBoxText;
+
+        [ObservableProperty]
+        private string currentTemp;
+
+        [RelayCommand]
+        public async Task SearchButtonPressed()
+        {
+            var dataInfo = await WeatherData.LoadData(SearchBoxText);
+
+            CurrentTemp = $"{dataInfo.current.temp_c}";
+        }
+
+
+    }
+}
